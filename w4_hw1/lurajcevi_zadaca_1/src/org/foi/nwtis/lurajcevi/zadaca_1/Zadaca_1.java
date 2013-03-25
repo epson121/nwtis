410ddeabe0097affce85ca8bbb39d33b383f2029
epson121 <traxdater12@gmail.com>
2013-03-25 23:11:46 +0100

    updated regex

--- w4_hw1/lurajcevi_zadaca_1/src/org/foi/nwtis/lurajcevi/zadaca_1/Zadaca_1.java
+++ w4_hw1/lurajcevi_zadaca_1/src/org/foi/nwtis/lurajcevi/zadaca_1/Zadaca_1.java
@@ -25,6 +25,8 @@ public class Zadaca_1 {
             boolean status;
             String regexServer = "^-server -port ([8-9]\\d{3}) -konf ([^\\s]+\\.(?i)txt|xml)( +-load)? -s ([^\\s]+\\.[^\\s]+) *$";
             String regexUser = "^-user -ts (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) -port ([8-9]\\d{3}) -u ([a-zA-Z]+) -konf ([a-zA-Z0-9_]+\\.(txt|xml))";
+            String regexShow = "^-show -s ([a-zA-Z0-9_]+\\.[a-zA-Z]+)";
+            String regexAdmin = "^-admin -ts (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) -port ([8-9]\\d{3}) -u ([a-zA-Z]+) -p ([a-zA-Z]+) -konf ([a-zA-Z0-9_]+\\.(txt|xml)) (-t (\\d\\d.\\d\\d.\\d\\d\\d\\d \\d\\d:\\d\\d:\\d\\d)|PAUSE|START|STOP|CLEAN)";
             StringBuilder sb = new StringBuilder();
             
             for (int i = 0; i < args.length; i++){
